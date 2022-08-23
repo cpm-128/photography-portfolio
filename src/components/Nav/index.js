a70c85953557c9ca7ade806690a8b7977e1a647b
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-23 15:52:43 -0400

    capitalize first letter helper added to nav and gallery

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav() {
 
@@ -53,7 +54,7 @@ function Nav() {
                     {categories.map((category) => (
                         <li className="mx-1" key={category.name}>
                             <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
