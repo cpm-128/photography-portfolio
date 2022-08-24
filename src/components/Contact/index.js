1f8662378cc8de32d43b02252f4569cf6a634f84
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-24 12:13:02 -0400

    syn data from form to state of the component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+
+    // hook to manage the data and sync the form data with the component's state
+    const [formState, setFormState] = useState(
+        // clear input field on the component loading
+        {
+            name: '',
+            email: '',
+            message: ''
+        }
+    );
+
+    //sync internal state of component formState with the user input from the vdom anything there is a keystroke in name
+    function handleChange(e) {
+        // spreadoperator to retain the other key-value pairs in the object
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+    console.log('>> formState >>', formState)
 
     // create the elements of the contact form to display on vdom
     return (
@@ -11,17 +28,17 @@ function ContactForm() {
                 {/* name input */}
                 <div>
                     <label htmlFor='name'>Name: </label>
-                    <input type='text' name='name'></input>
+                    <input type='text' name='name' defaultValue={formState.name} onChange={handleChange}></input>
                 </div>
                 {/* email input */}
                 <div>
                     <label htmlFor='email'>Email address: </label>
-                    <input type='email' name='email'></input>
+                    <input type='email' name='email' defaultValue={formState.email} onChange={handleChange}></input>
                 </div>
                 {/* message text area */}
                 <div>
                     <label htmlFor='message'>Message: </label>
-                    <textarea name='message' rows='5'></textarea>
+                    <textarea name='message' rows='5' defaultValue={formState.message} onChange={handleChange}></textarea>
                 </div>
                 {/* button */}
                 <button type='submit'>Submit</button>
